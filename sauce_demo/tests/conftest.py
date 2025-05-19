d5fb09a2e01c0273bc0f062a8f74ab119b47de03
LalaEd <edinlaletovic@gmail.com>
2025-05-19 16:47:18 +0200

    Fix: check error message when accessing inventory without login

--- sauce_demo/tests/conftest.py
+++ sauce_demo/tests/conftest.py
@@ -1,8 +1,8 @@
 import pytest
 
-
 @pytest.fixture()
 def setup_tear_down(page) -> None:
     page.set_viewport_size({"width": 1920, "height": 606})
     page.goto("https://www.saucedemo.com/")
     yield page
+
